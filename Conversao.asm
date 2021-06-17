2192d0f3fd43160d1428541fcc8e4ad95ad357d1
Marco Toledo <marcoantonioribeirodetoledo@gmail.com>
2021-06-17 20:15:22 -0300

    Implementadas conversões a partir de binário e hexa

--- Conversao.asm
+++ Conversao.asm
@@ -11,8 +11,9 @@
 	debug: .asciiz ":::::: "
 	quebraDeLinha: .asciiz "\n"
 	promptConvertido: .asciiz "Valor convertido: "
+	hexadec: .asciiz "0x"
 	stringLida: .space 64
-	
+
 .text
 
 .globl main
@@ -25,11 +26,11 @@ main:
 	li $v0, 4
 	la $a0, inicioPrompt
 	syscall
-	
+
 	# Lê string (idealmente nula)
 	li $v0, 8
 	syscall
-	
+
 	# Prompt de entrada de base
 	li $v0, 4
 	la $a0, baseEntrada
@@ -59,7 +60,7 @@ numeroInvalido:
 	syscall
 	
 	j fim
-	
+
 # Lê base para saída, conferindo se base inválida (igual a de entrada) em $t7
 promptSaida:
 	li $v0, 4
@@ -82,18 +83,19 @@ promptSaida:
 		# Finaliza program
 		j fim
 
-converteValor:
+# Transforma valor em $t5 de caracater [0...9, 'a', 'b', 'c', 'd', 'e', 'f'] para inteiro
+converteValor:	
 	# Se não for carater de número
-	bgt $t5, 58, ehLetra
+	bgt $t5, 58, ehLetraConv
 	# Menor que '0'
-	ble $t5, 48, numeroInvalido
+	ble $t5, 47, numeroInvalido
 	
 	addi $t5, $t5, -48
 	j endConvVal
 	
 	ehLetraConv:
 		ble $t5, 96 , numeroInvalido 	# Checa se é letra realmente
-		bgt $t5, 102, numeroInvalido	# Checa se as letras são a,b,c,d,e,f
+		bgt $t5, 102, numeroInvalido	# Checa se as letras são a, b, c, d, e f
 		addi $t5, $t5, -87
 		
 	endConvVal:
@@ -101,10 +103,17 @@ converteValor:
 
 # Fim de programa
 fim:
+	li $v0, 4
+	la $a0, quebraDeLinha
+	syscall
+
 	# Chamada de sistema de fim de programa
 	li $v0, 10
 	syscall
 
+## Entrada binária ##
+
+# Lê entrada binária
 entradaBin:
 	# Imprime string para entrada de dados
 	li $v0, 4
@@ -117,12 +126,143 @@ entradaBin:
 	li $a1, 64
 	syscall
 	
+	li $t7, 2
+	jal promptSaida
+	move $s0, $v0
+	
+	converteParaInt:
+		li $t5, 1 # Contador para exponencial
+		la $t3, stringLida
+		move $a0, $t3
+		li $t4, 64
+		li $t9, 0 # Valor decimal inicial
+	
+	beq $s0, 10, binToDec
+	beq $s0, 16, binToHex
+	
+	j numeroInvalido
+
+binToDec:
+	converteByteBin:
+		lb $t2, ($t3) # Carrega caracter da string
+	
+		beq $t2, '\n', printDecimalBin
+		bgt $t2, 49, numeroInvalido
+		blt $t2, 48, numeroInvalido
+		
+		subi $t2, $t2, 48 # Transforma ascii para 0/1
+		beq $t2, 0, ehZeroBin # Caso 0
+		beq $t2, 1, ehUmBin # Caso 1
+		j printDecimalBin
+	
+	ehZeroBin:
+		addi $t3, $t3, 1	# Byte offset++
+		subi $t4, $t4, 1	# Contador--
+		j converteByteBin
+	
+	ehUmBin:
+		# Adiciona exponencial do contador
+		addi $t3, $t3, 1	# Byte offset++
+		subi $t4, $t4, 1	# Contador--
+		sllv $t6, $t5, $t4
+		add $t9, $t9, $t6	# Computa valor da exponencial para $t9
+		j converteByteBin
+	
+	printDecimalBin:
+		srlv $t9, $t9, $t4	
+				# Shift de $t4 << contador concertando posição de byte
+				# Exemplo: convertendo (11)_2 -> 11000000000...
+				# então, para consertar, fazemos o shift para ...00000000000011, obtendo (3)_10
+	
+		# Mensagem de saída
+		la $a0, promptConvertido
+		li $v0, 4
+		syscall
+	
+		# Exibe valor convertido
+		move $a0, $t9
+		li $v0, 1
+		syscall
+	
+	j fim
+	
+binToHex:
+	converteByteHex:
+		lb $t2, ($t3) # Carrega caracter da string
+	
+		beq $t2, '\n', valBinHex
+		bgt $t2, 49, numeroInvalido
+		blt $t2, 48, numeroInvalido
+		
+		subi $t2, $t2, 48 # Transforma ascii para 0/1
+		beq $t2, 0, ehZeroHex # Caso 0
+		beq $t2, 1, ehUmHex # Caso 1
+		j valBinHex
+
+	ehZeroHex:
+		addi $t3, $t3, 1	# Byte offset++
+		subi $t4, $t4, 1	# Contador--
+		j converteByteHex
+		
+	ehUmHex:
+		# Computa exponencial do contador
+		addi $t3, $t3, 1	# Byte offset++
+		subi $t4, $t4, 1	# Contador--
+		sllv $t6, $t5, $t4
+		add $t9, $t9, $t6	# Adiciona resultado do exponencial para $t9
+		j converteByteHex
+
 	j fim
 
+valBinHex:
+	srlv $t9, $t9, $t4	# Concerta bytes decimais
+	
+	move $t0, $t9 # Salva inteiro de $t9 em $t0
+	li $t2, 15
+	li $t1, 28
+	
+	# Exibe mensagem de saída
+	la $a0, promptConvertido
+	li $v0, 4
+	syscall
+	
+	# Exibe incício de hexa
+	la $a0, hexadec
+	li $v0, 4
+	syscall
+
+startLoopBinHex:
+	srlv $a0, $t0, $t1  	#parcial = numeroGrande / 2^i, sendo que i é sempre um multiplo de 4   
+	and $a0, $a0, $t2	#parcial = parcial%16
+	li $v0, 1 		#Para a syscall
+	bge $a0, 10, printStringBinHex #if(parcial> 9)goto printString 
+	
+returnFromPrintStringBinHex:		#prints parcial as a number, depending on its value (<10 or >10)
+	syscall			#imprime parcial como numero ou caractere dependendo se parcial é menor que 10 ou nao
+	addi $t1, $t1, -4	#i = i-4;
+	bne $t1, -4, startLoopBinHex	#}while(i >= 0)
+	
+	j fim #return
+	 
+	#fix values from $a0 and $v0 to print in hex(10 = A, 11 = B, 12 = C, 13 = D, 14 = E, 15 = F)
+printStringBinHex:
+	addi $a0, $a0, 87 # 'a' é 87 no ASCII. $a0 == 10 é pra ser 'a', então somamos 87 no valor de $a0 e imprimimos como caracter
+	li $v0, 11 # Print $v0 como caracter
+	j returnFromPrintStringBinHex
+
+## Entrada binária ##
+
+# Lê valor decimal
 entradaDec:
 	# Imprime string para entrada de dados
 	li $v0, 4
 	la $a0, valorEntrada10
+	syscall
+	
+	# Lê entrada como string
+	li $v0, 8
+	la $a0, stringLida
+	li $a1, 64
 	syscall
 	
 	j fim
@@ -161,16 +301,15 @@ entradaHex:
 	
 	move $s0, $v0
 	
-	beq $s0, 2, hexToBin
 	beq $s0, 10, hexToDec
-	
+	beq $s0, 2, hexToDec
 	j numeroInvalido
 
 hexToDec:
 	move $t0, $zero
-	move $t5, $zero
 	enquantoNum:
-		lb $t5, stringLida($t5)
+		lb $t5, stringLida($t0)
+		beq $t5, '\n', fimHexToDec
 		addi $t0, $t0, 1
 		
 		jal converteValor
@@ -190,18 +329,53 @@ hexToDec:
 			add $s7, $s7, $t5
 			j enquantoNum
 	
-	li $v0,4
-	la $a0, promptConvertido
-	syscall
+	fimHexToDec:
+		beq $s0, 2, hexToBin
 	
-	# Printar valor de dec unsigned
-	li $v0, 36
-	move $a0, $s7
-	syscall
+		li $v0,4
+		la $a0, promptConvertido
+		syscall
 	
-	j fim
+		# Printar valor de dec unsigned
+		li $v0, 36
+		move $a0, $s7
+		syscall
+	
+		j fim
 	
 hexToBin:
+	li $t0, 0 	# Inicia index no 0
+	li $t2, 2	# Valor 2 para dividir
+	ateZerarQuotient:
+		divu $s7, $t2		# Divide por 2 para achar cada digito  do binário
+ 		
+		mflo $s7
+		mfhi $t3
+		
+		addiu $sp, $sp, -1	# Salva para stack cada byte do módulo
+		sb $t3,($sp)
+		
+		addi $t4, $t4, 1	# Incrementa para usarmos depois quando formos printar a string de trás para frente
+		
+		bnez $s7, ateZerarQuotient
+	
+	# Printar mensagem para resultado bin
+	li $v0, 4	
+	la $a0, promptConvertido
+	syscall
+		
+	ateFinalString:			#escreve a string de trás para frente
+		lb $t3,($sp)		#carrega da stack
+		addi $sp, $sp, 1
+	
+		addi $t4, $t4, -1	# Contador bits
+	
+		li $v0, 1		# Printar valor de bin
+		move $a0, $t3
+		syscall
+	
+		bgtz $t4, ateFinalString
+
 	j fim
 
 # Confere se caracter atual ($t5) é número ou letra entre ['a', 'f']
@@ -209,7 +383,7 @@ confereHex:
 	# Se não for carater de número
 	bgt $t5, 58, ehLetra
 	# Menor que '0'
-	ble $t5, 48, numeroInvalido
+	ble $t5, 47, numeroInvalido
 	
 	j endConfHex
 	
